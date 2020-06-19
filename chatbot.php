577040a06fb2ea134b5e8a366a7ad213c1697f06
Jappie313 <66782104+Jappie313@users.noreply.github.com>
2020-06-19 16:33:56 +0200

    Added more answers + little bit of css updates

--- chatbot.php
+++ chatbot.php
@@ -11,6 +11,7 @@
     //TODO add more questions and answers!
     //TODO add css for new forms
     session_start();
+    $urlKook = "https://www.leukerecepten.nl/";
     //base functions
     function getCMessage(){
         if(isset($_POST["submit"])) {
@@ -25,10 +26,13 @@
         echo $response;
     }
     function findSResponse($input){
-       if(preg_match("/help/i",$input))return nl2br("test 1 2 3\n");
+       if(preg_match("/help/i",$input))return nl2br("als je je verveelt kunnen wij je helpen. Type: 'ik verveel me' voor ideeën om te doen\n");
        else if ($input === "ERROR: No message was send!")return nl2br("ERROR: No message was send!\n");
        else if (preg_match("/verveel/i",$input))return nl2br("Ga een stukje wandelen of ga sporten.\n Maak je email inbox op orde. Meld je of voor reclame en spam.\n Bel of stuur een bericht naar iemand die je al een tijdje niet meer hebt gesproken.\n Maak eindelijk die kamer van je een schoon. Al dat stof is niet gezond voor je.\n Ga lekker een spanned boek lezen of begin aan een nieuwe netflix serie.");
        else if (preg_match("/doen/i",$input)) return nl2br("Ga eten koken voor de komende dagen. Dan heb je dat alvast gedaan.\n Maak een buckit list.\n Als je nog geen vakantie hebt voor de vakantie. Ga opzoek naar een leuke locatie en plan een vakantie daar heen,\n Ga met een paar vrienden of familie naar een museum of de bioscoop.\n Ga mensen op een forum helpen met hun problemen.\n of nodig een paar vrienden uit of te gaan chillen.");
+       else if (preg_match("/kook/i",$input)) return nl2br("Dit is een goed site meet veel ideeën om wat lekkers te koken: ".$GLOBALS["urlKook"]);
+       else if (preg_match("/vakantie/i",$input)) return "Je kan hier goede vakantie ideeën opdoen: https://www.skyscanner.nl/nieuws/inspiratie/10-ideeen-voor-een-super-zonnige-vakantie";
+       else if (preg_match("/boek/i",$input)) return "Je kan heel veel leuke boeken op deze site vinden: https://www.bruna.nl/boekentop100";
        else {
            return "Ik weet niet hoe ik niet op moet antwoorden.<br> Jouw vraag was \"$input\".<br>".
                   '<form method="post">
@@ -73,19 +77,18 @@
             }
             else if (isset($_POST["addSubmit"])) echo "Vul beide tekst velden in.";
     }
-    echo $_SESSION["age"];
+
     ?>
     <div id="aboveChatbot">
         <h1 class="title">Welkom bij de "een ChatBot is geen ChatRobot en al helemaal geen RobotChat" chatbot.</h1>
+        <p class="intro">Deze chatbot kan je helpen om op leuke ideeën te komen als je je verveelt! <br>Zo kan je "Ik verveel me." of "Wat moet ik doen?" typen om ideeën te krijgen</p>
     </div>
     <div class="chatbot">
-        <div class="chatdiv"><p class="output"><?php echo nl2br("Hallo ".$_SESSION["name"]."\n"); echo echoInput(); addQuestion(); writeQ_A();?><?php if (isset($_POST["submit"])) sendSResponse(findSResponse(getCMessage()));?></p></div>
+        <div class="chatdiv"><p class="output"><?php echo nl2br("Hallo ".$_SESSION["name"]."(".$_SESSION["age"].")\n"); echo echoInput(); addQuestion(); writeQ_A();?><?php if (isset($_POST["submit"])) sendSResponse(findSResponse(getCMessage()));?></p></div>
             <form id="chatbotForm" method="post">
                 <input class="input" name="cMessage" type="text" placeholder="Your Message">
                 <input class="submit" type="submit" name="submit"/>
             </form>
-
-
     </div>
 
 </body>
