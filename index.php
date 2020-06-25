20a47cc4abbc4e35611aa9a04da2350d058cad52
WAlbeda <59028184+walbeda@users.noreply.github.com>
2020-06-25 15:26:52 +0200

    Corrected a tab and removed some enters.

--- index.php
+++ index.php
@@ -10,30 +10,28 @@
     <script>alert("Let op!\nDe gegevens die u invuld op deze website worden opgeslagen op onze server.\nGebruik de website op eigen risico.\n\nEN:\nWarning!\nThe information you submit on this website is saved on our server.\nUse the website at your own risk.")</script>
 </head>
 <body>
-<?php
-    //Open code made by: Willem Albeda and Jappie313.
-    session_start();
-    if(isset($_POST["submit"])){
-        $firstName = "Name: ".$_POST["firstName"]." ";
-        $surName = $_POST["surName"]."\n";
-        $email = "Email: ".$_POST["email"]."\n";
-        $day = "birth day: ".$_POST["day"];
-        $month = " ".$_POST["month"];
-        $year = " ".$_POST["year"]."\n";
-        $all = "$firstName $surName $email $day $month $year";
-        file_put_contents("data.txt",$all,FILE_APPEND);
-        header("Location: chatbot.php");
+    <?php
+        //Open code made by: Willem Albeda and Jappie313.
+        session_start();
+        if(isset($_POST["submit"])){
+            $firstName = "Name: ".$_POST["firstName"]." ";
+            $surName = $_POST["surName"]."\n";
+            $email = "Email: ".$_POST["email"]."\n";
+            $day = "birth day: ".$_POST["day"];
+            $month = " ".$_POST["month"];
+            $year = " ".$_POST["year"]."\n";
+            $all = "$firstName $surName $email $day $month $year";
+            file_put_contents("data.txt",$all,FILE_APPEND);
+            header("Location: chatbot.php");
 
-        $_SESSION["name"] = $_POST["firstName"] . " " . $_POST["surName"];
-        $birthDate = $_POST["month"] . "/" . $_POST["day"] . "/" . $_POST["year"];
-        $birthDate = explode("/", $birthDate);
-        $_SESSION["age"] = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md")
-            ? ((date("Y") - $birthDate[2]) - 1)
-            : (date("Y") - $birthDate[2]));
-    }
-
-
-?>
+            $_SESSION["name"] = $_POST["firstName"] . " " . $_POST["surName"];
+            $birthDate = $_POST["month"] . "/" . $_POST["day"] . "/" . $_POST["year"];
+            $birthDate = explode("/", $birthDate);
+            $_SESSION["age"] = (date("md", date("U", mktime(0, 0, 0, $birthDate[0], $birthDate[1], $birthDate[2]))) > date("md")
+                ? ((date("Y") - $birthDate[2]) - 1)
+                : (date("Y") - $birthDate[2]));
+        }
+    ?>
     <div id="aboveChatbot">
         <h1 class="title">Dit is geen ROBOCHAT.</h1>
         <p class="intro">Deze chatbot is een wiki geïnspireerd op codepanta.</p>
